12460b8b2c0c4e53f8b633a47ed840dde25e2259
Giovanna Jesus <giovannajesusborba23@gmail.com>
2026-03-05 01:11:54 -0300

    Atualizando index.html com formulário, adicionando dependência jackson e retornando json após envio do formulário

--- src/main/java/br/com/bookstore/servlet/CreateBookServlet.java
+++ src/main/java/br/com/bookstore/servlet/CreateBookServlet.java
@@ -1,9 +1,25 @@
 package br.com.bookstore.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException; 
 
 @WebServlet("/create-book")
 public class CreateBookServlet extends HttpServlet {
 
+    @Override
+    protected  void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+
+        String bookTitle = request.getParameter("book-title");
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String json = "{\"title\": \"" + bookTitle + "\"}";
+
+        response.getWriter().write(json);
+    }
 }
